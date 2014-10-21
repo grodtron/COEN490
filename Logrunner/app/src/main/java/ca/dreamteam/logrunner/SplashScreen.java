7623ad65b99abd1b72fc62cc11fd28cbcb6068a5
AmineMhedhbi <m.amine.mhedhbi@gmail.com>
2014-10-21 03:52:53 -0400

    splash screen + Main menu + BarAction(Settings) set

---
+++ Logrunner/app/src/main/java/ca/dreamteam/logrunner/SplashScreen.java
@@ -0,0 +1,42 @@
+package ca.dreamteam.logrunner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+public class SplashScreen extends Activity {
+
+    private static final int SPLASHTIME = 2000;
+    private static final int STOPSPLASH = 0;
+    private Handler mSplashHandler;
+
+    // Handler start
+    {
+        mSplashHandler = new Handler()
+        {
+            @Override
+            public void handleMessage(Message msg)
+            {
+                super.handleMessage(msg);
+                switch (msg.what)
+                {
+                    case STOPSPLASH:
+                        Intent intent = new Intent(getApplicationContext(), Main.class);
+                        startActivity(intent);
+                        finish();
+                }
+            }
+        };
+    }// Handler end
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        // to start the handler
+        mSplashHandler.sendEmptyMessageDelayed(STOPSPLASH, SPLASHTIME);
+    }
+}
