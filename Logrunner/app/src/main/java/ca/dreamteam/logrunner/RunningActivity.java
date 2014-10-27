baec425a07688eb6b7473cf07636fefc2796fe8f
AmineMhedhbi <m.amine.mhedhbi@gmail.com>
2014-10-26 21:07:30 -0400

    changes to running activity

--- Logrunner/app/src/main/java/ca/dreamteam/logrunner/RunningActivity.java
+++ Logrunner/app/src/main/java/ca/dreamteam/logrunner/RunningActivity.java
@@ -1,7 +1,6 @@
 package ca.dreamteam.logrunner;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,9 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.Button;
-import android.widget.ImageView;
 
 
 public class RunningActivity extends Activity {
@@ -60,9 +57,12 @@ public class RunningActivity extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_running, container, false);
-            final Button endRunButton = (Button) rootView.findViewById(R.id.endrun_button);
 
-            endRunButton.setOnClickListener(new View.OnClickListener() {
+            Button StopButton = (Button) rootView.findViewById(R.id.stop_button);
+
+
+
+            StopButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent(getActivity(), FinishRunActivity.class);
