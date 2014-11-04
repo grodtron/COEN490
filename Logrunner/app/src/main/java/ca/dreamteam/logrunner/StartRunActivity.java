a74f7151dabae3f984955f06842d5e153d307583
AmineMhedhbi <m.amine.mhedhbi@gmail.com>
2014-11-04 00:54:01 -0500

    Get Chrono to work

--- Logrunner/app/src/main/java/ca/dreamteam/logrunner/StartRunActivity.java
+++ Logrunner/app/src/main/java/ca/dreamteam/logrunner/StartRunActivity.java
@@ -2,6 +2,7 @@ package ca.dreamteam.logrunner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
@@ -11,33 +12,37 @@ public class StartRunActivity extends Activity {
 
     final String TAG = StartRunActivity.this.getClass().getSimpleName();
 
-    private Chronometer chronometer;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_run);
 
         Button runButton = (Button) findViewById(R.id.runButton);
+        final Chronometer chronometer = (Chronometer) findViewById(R.id.mChronometer);
 
         runButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 TextView textButton = (TextView) findViewById(R.id.textButton);
                 Button tempButton = (Button) findViewById(R.id.runButton);
                 // Based on the textButton value change between Run, Stop & Save actions
                 if (((String)textButton.getText()).compareTo("START RUN") == 0) {
                     textButton.setText("STOP");
                     tempButton.setBackgroundColor(android.graphics.Color.RED); // Blue
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    chronometer.start();
                 } else if (((String)textButton.getText()).compareTo("STOP") == 0) {
+                    //chronometer.stop();
                     textButton.setText("SAVE");
                     tempButton.setBackgroundColor(android.graphics.Color.parseColor("#33B5E5"));
+                    chronometer.stop();
                 } else if (((String)textButton.getText()).compareTo("SAVE") == 0) {
                     textButton.setText("SAVE COMPLETE!");
                     // intent to do the NEXT thing
                 }
 
-                // Each point should run on a thread but not the UI thread
+                // Each point should run on a thread but not the UI thread (it's own class?)
                 // * Read humidity twice at start and end
                 // * Read temperature, pressure with same freq on sensor
                 // * Update distance
---
+++ nice to have Features.md
@@ -0,0 +1,7 @@
+# Features to work on after 1st release:
+
+## Make the Chronometer with millisecond–Custom component
+a Timer
+a TimerTask
+a class that implements the Runnable interface
+& a custom component
